0319741962f6f0c78c2741fde79a70deff543862
dekevinquint <dekevinquint@gmail.com>
2026-07-21 22:38:28 +0200

    Add viewport-fit=cover and maximumScale for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,8 @@ export const viewport: Viewport = {
   themeColor: "#14201e",
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  viewportFit: "cover",
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
