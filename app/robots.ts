c386d053d8c50ec622081255dcf96e221994647e
dekevinquint <dekevinquint@gmail.com>
2026-07-24 22:03:58 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
                   userAgent: '*',
                   allow: '/',
           },
-          sitemap: 'https://aloud-reader-one.vercel.app/sitemap.xml',
+                  sitemap: 'https://www.hearaloud.com/sitemap.xml',
     }
 }
