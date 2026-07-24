2f880f10a9b5804d02ab5d420df7ddb697b10db3
dekevinquint <dekevinquint@gmail.com>
2026-07-24 22:01:47 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
       {
-              url: 'https://aloud-reader-one.vercel.app',
+                      url: 'https://www.hearaloud.com',
               lastModified: new Date(),
               changeFrequency: 'monthly',
               priority: 1,
